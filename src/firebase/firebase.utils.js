74944aca5159b035aab5be74736b3a62f0a9faf7
guitarrockstarco <guitarrockstarco@yahoo.com>
2020-02-08 16:58:00 -0500

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop pages.  Also included with our spinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,16 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyBjA6i851UVil9h9UwPipvs8fvK-Li7nr8",
-  authDomain: "crwn-db-b5eed.firebaseapp.com",
-  databaseURL: "https://crwn-db-b5eed.firebaseio.com",
-  projectId: "crwn-db-b5eed",
-  storageBucket: "crwn-db-b5eed.appspot.com",
-  messagingSenderId: "42469355047",
-  appId: "1:42469355047:web:f3eb2296cf2d8c5705e802",
-  measurementId: "G-YQ6EG3ZFQ4"
+  apiKey: 'AIzaSyBjA6i851UVil9h9UwPipvs8fvK-Li7nr8',
+  authDomain: 'crwn-db-b5eed.firebaseapp.com',
+  databaseURL: 'https://crwn-db-b5eed.firebaseio.com',
+  projectId: 'crwn-db-b5eed',
+  storageBucket: 'crwn-db-b5eed.appspot.com',
+  messagingSenderId: '42469355047',
+  appId: '1:42469355047:web:f3eb2296cf2d8c5705e802',
+  measurementId: 'G-YQ6EG3ZFQ4'
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
@@ -18,9 +18,9 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapShot = await userRef.get()
+  const snapShot = await userRef.get();
 
-  if(!snapShot.exists) {
+  if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
@@ -30,14 +30,46 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         email,
         createdAt,
         ...additionalData
-      })
+      });
     } catch (error) {
       console.log('error creating user', error.message);
     }
   }
 
   return userRef;
-}
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transforedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+  return transforedCollection.reduce((accumulator, collections) => {
+    accumulator[collections.title.toLowerCase()] = collections;
+    return accumulator;
+  }, {});
+};
 
 firebase.initializeApp(config);
 
